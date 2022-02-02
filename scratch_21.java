c2da6842a586d7927e54e25aa62424c39ac0abbd
Egor-o-a <egor.o.a@mail.ru>
2022-02-02 21:57:11 +0300

    adding decisions for 18-th, 19-th, 20-th, 21-th, 22-th, 23-th and 24-th

---
+++ scratch_21.java
@@ -0,0 +1,54 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class scratch_21 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    //напишите тут ваш код
+
+    public static void universalMethod(int chi) {
+
+    }
+
+    public static void universalMethod(int chi1, int chi2) {
+
+    }
+
+    public static void universalMethod(int[] arrayChi) {
+
+    }
+
+    public static void universalMethod(int[][] twoDimensionalArrayChi) {
+
+    }
+
+    public static void universalMethod(int[][][] multidimensionalArrayChi) {
+
+    }
+
+    public static void universalMethod(String str) {
+
+    }
+
+    public static void universalMethod(boolean blo) {
+
+    }
+
+    public static void universalMethod(float flo) {
+
+    }
+
+    public static void universalMethod(char cha) {
+
+    }
+}
---
+++ scratch_23.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task06.task0608;
+
+/* 
+Кубический калькулятор
+*/
+
+public class scratch_23 {
+    public static void main(String[] args) {
+
+    }
+
+    //напишите тут ваш код
+
+    public static long cube(long chi) {
+        return (chi * chi * chi);
+    }
+}
